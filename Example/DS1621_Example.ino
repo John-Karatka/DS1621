6061b00531cbb0069466167ce9ad848171758ba3
John-Karatka <john.karatka@gmail.com>
2018-03-06 12:57:35 -0500

    Create DS1621_Example.ino

---
+++ Example/DS1621_Example.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <DS1621.h>
+
+#define address 0	//chip address
+
+TempSense tmp(address);
+
+void setup() {
+Wire.begin();
+Serial.begin(9600);
+}
+
+void loop() {
+signed int temperature = tmp.getTempF;
+
+Serial.println(temperature);
+delay(1000);
+}
